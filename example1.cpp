145b7c833c70129ae4023db4e72cd3e89236bb65
Pyungkang Kim <21500186@handong.edu>
2017-10-30 13:09:19 +0000

    hh..

--- example1.cpp
+++ example1.cpp
@@ -5,7 +5,7 @@ class Crain : public CraneCrane
 {
 private:
     ev3dev::touch_sensor touch_q;
-    ev3dev::color_sensor color_q;
+    ev3dev::color_sensor colorSencor;
     ev3dev::motor a;
     ev3dev::motor b; 
     ev3dev::motor c;
@@ -101,47 +101,14 @@ void Crain::example_code()
 { //This function is for example, you should develop your own logics
     while(get_escape() == false)
     {
-        a.right;
-        set_down(ev3dev::button::down.pressed());
-        set_up(ev3dev::button::up.pressed());
-        set_right(ev3dev::button::right.pressed());
-        set_left(ev3dev::button::left.pressed());
-        set_escape(ev3dev::button::back.pressed());
-        set_enter(ev3dev::button::enter.pressed());
+      
+    
         
-        if(get_up())
-        {   
-                a.set_speed_sp(-1*get_speed());
-                a.run_forever();
-        }   
-        if(get_down())
+        if (colorSensor.raw(0,0,0))
         {
-                a.set_speed_sp(get_speed());
-                a.run_forever();
-        }
-        if(get_left())
-        {
-               b.set_speed_sp(get_speed());
-               b.run_forever();
-        }
-        if(get_right())
-        {
-               b.set_speed_sp(-1* get_speed());
-               b.run_forever();
-        }
-       
-       
-        if(!(get_up() | get_down() | get_right() | get_left() | get_enter()))
-        {
-            a.set_speed_sp(0);
-            a.run_forever();
-            b.set_speed_sp(0);
-            b.run_forever();
+            ev3dev::sound::speak("HI");
         }
     }
-
-    a.stop();
-    b.stop();
 }
 
 int main()
