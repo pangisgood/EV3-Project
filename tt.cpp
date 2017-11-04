5436f3a3fd1a1bb8b116af6d918f8db59e2a3b51
Pyungkang Kim <21500186@handong.edu>
2017-11-04 07:31:34 +0000

    mys

--- tt.cpp
+++ tt.cpp
@@ -1,358 +1,3 @@
-#include "h_crane.h"
-class Crain : public CraneCrane
-
-{
-
-private:
-
-    ev3dev::touch_sensor touch_q;
-
-    ev3dev::motor a;
-
-    ev3dev::motor b; 
-
-    ev3dev::motor c;
-
-    ev3dev::color_sensor colorSensor;
-
-    ev3dev::ultrasonic_sensor ultra_q;
-
-    
-
-public:
-
-    // Hardware Configuration. 초기화 객체에 알맞은 포트 넣어줌. 
-
-    Crain():m_speed(0), touch_q(ev3dev::INPUT_1),a(ev3dev::OUTPUT_A), b(ev3dev::OUTPUT_B), c(ev3dev::OUTPUT_C), ultra_q(ev3dev::INPUT_2)
-
-    
-
-    {
-
-        
-
-    }
-
-    int m_speed, pose, flag;
-
-    
-
-    double get_distance()
-
-    {
-
-        double dis;
-
-        
-
-        return ultra_q.distance_centimeters();
-
-        
-
-    }
-
-    bool is_black()
-
-    {
-
-        return colorSensor.color() == 1;  //검정색이면 1==1 만족, 트루 반환?
-
-    }
-
-    
-
-    bool get_touch_pressed()
-
-    {
-
-        return touch_q.is_pressed();
-
-    }
-
-    
-
-    virtual bool get_down()
-
-    {
-
-        return m_down; //처음엔 다 false로 되어있음. 
-
-    }
-
- 
-
-    virtual bool get_up()
-
-    {
-
-        return m_up;
-
-    }
-
- 
-
-    virtual bool get_right()
-
-    {
-
-        return m_right;
-
-    }
-
- 
-
-    virtual bool get_left()
-
-    {
-
-        return m_left;
-
-    }
-
- 
-
-    virtual bool get_enter()
-
-    {
-
-        return m_enter;
-
-    }
-
- 
-
-    virtual bool get_escape()
-
-    {
-
-        return m_escape;
-
-    }
-
-    
-
-    virtual int get_speed()
-
-    {
-
-        return 780;
-
-    }
-
-    
-
-    virtual int get_speed_neck()
-
-    {
-
-        return 660;
-
-    }
-
-    // virtual int  get_speed_foot()
-
-    // {
-
-    //     return 600;
-
-    // }
-
-    
-
-    // virtual int  get_speed_neck()
-
-    // {
-
-    //     return 300;
-
-    // }
-
-    
-
-    virtual int get_speed_hand()
-
-    {
-
-        return 400;
-
-    }
-
- 
-
-    virtual void set_down(bool val)
-
-    {
-
-        m_down = val;
-
-    }
-
-    
-
-    virtual void set_up(bool val)
-
-    {
-
-        m_up = val;    
-
-    }
-
-    
-
-    virtual void set_right(bool val)
-
-    {
-
-        m_right = val;   
-
-    }
-
-    virtual void set_left(bool val)
-
-    {
-
-        m_left = val; 
-
-    } 
-
-    
-
-    virtual void set_enter(bool val)
-
-    {
-
-        m_enter = val;    
-
-    }
-
-    
-
-    virtual void set_escape(bool val)
-
-    {
-
-        m_escape = val;    
-
-    }
-
-    
-
-    virtual void set_speed(int val)
-
-    {
-
-        m_speed = val;    
-
-    }
-
-    
-
- 
-
-    
-
-public:
-
-    void example_code();
-
-    void move_foot_rfr();
-
-    void stop_foot_rfr();
-
-    void move_foot(int pos);
-
-    void move_neck(int pos);
-
-    void move_hand(int pos);
-
-    void reset_motors();
-
-    int position_foot();
-
-    int position_neck();
-
-    int position_hand();
-
-    void practice();
-
-    void getbackAuto();
-
-    void execute();
-
-    
-
-};
-
- 
-
-void Crain::move_foot_rfr()
-
-{
-
-    c.set_speed_sp(100);
-
-    c.run_forever();
-
- 
-
-}
-
- 
-
-void Crain::stop_foot_rfr()
-
-{
-
-    c.set_stop_action("hold");
-
-    c.stop();
-
-}
-
- 
-
- 
-
- 
-
-void Crain::getbackAuto()
-
-{
-
-    a.set_speed_sp(100);
-
-    a.set_position_sp(0);
-
-    a.run_to_abs_pos();
-
-    sleep(1);
-
-    
-
-    b.set_speed_sp(100);
-
-    b.set_position_sp(0);
-
-    b.run_to_abs_pos();
-
-    sleep(1);
-
-    
-
-    c.set_speed_sp(100);
-
-    c.set_position_sp(0);
-
-    c.run_to_abs_pos();
-
-}
-
- 
-
- 
-
- 
-
 void Crain::reset_motors()
 
 {
@@ -628,22 +273,13 @@ void Crain::execute()
                 
 
             }
-//스캔해서 잡고 올리는 과정
+
             stop_foot_rfr();
 
+            sleep(slT);
 
-////x?
-           // sleep(0.5);
+            move_foot(position+35);
 
-            if(position>230){
-            move_foot(position+39);
-}
-
-
-            else{ move_foot(position+10);}
-            
-            
- 
             sleep(1.0);
 
             move_neck(max_neck);
@@ -662,11 +298,11 @@ void Crain::execute()
 
             sleep(slT);
 
-            move_neck(max_neck); //내려놓기
+            move_neck(max_neck);
 
-            sleep(0.7);
+            sleep(slT);
 
-            move_hand(0); //떨구기
+            move_hand(0);
 
             
 
@@ -674,26 +310,17 @@ void Crain::execute()
 
             
 
-            sleep(slT); 
+            sleep(slT);
 
-            move_neck(0); //목들기
+            move_neck(0);
 
             sleep(slT);
-            
-             if(flag == 2){
-                    move_foot(position); 
-                 
-             }
-            
-            else{
+
             move_foot(position-50);  //runforever 끝지점이 0인가?
-            }
-            
-            
-           
+
             sleep(1);
 
-           
+            
 
             flag += 1; 
 
